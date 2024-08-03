d3878be486700aeb686e642e1b0920571a47c2de
Prak EnTech <62505369+PrakEntech@users.noreply.github.com>
2024-08-03 19:38:32 +0530

    Update pokemondata.py

--- pokemondata.py
+++ pokemondata.py
@@ -1,53 +1,119 @@
-cm=[["Flare Blitz",90,70,2,"Recoil - 33%"],
+cm=[["Flare Blitz",90,70,2,"\nRecoil - 33%"],
    ["Dragon Rage",0.4,90,5,""],
    ["Wing Attack",60,90,3,""],
-   ["Fire Fang",65,85,5,"Burned - 30%"]]
-Charizard = {"speed":100, "HP":245,"HP_Cur":245, "Condition":"Normal", "Type":"Fire","Moves":cm}
+   ["Fire Fang",65,85,5,"\nBurned - 30%"]]
+Charizard = {"speed":100, "HP":245,"HP_Cur":245, "Type":"Fire","Moves":cm}
 
 im=[["Mach Punch",40,90,5,""],
-   ["Flame Wheel",60,90,5,"Burned - 10%"],
+   ["Flame Wheel",60,90,5,"\nBurned - 10%"],
    ["Fire Spin",35,85,4,""],
    ["Acrobatics",55,90,3,""]]
-Infernape = {"speed":108, "HP":242,"HP_Cur":242, "Condition":"Normal", "Type":"Fire","Moves":im}
+Infernape = {"speed":108, "HP":242,"HP_Cur":242, "Type":"Fire","Moves":im}
 
 mm=[["Hyper Beam",90,70,1,""],
-   ["Fire Punch",75,90,4,"Burned - 10%"],
+   ["Fire Punch",75,90,4,"\nBurned - 10%"],
    ["Ember",40,100,5,""],
-   ["Lava Plume",80,80,4,"Burned - 30%"]]
-Magmortar = {"speed":83, "HP":240,"HP_Cur":240, "Condition":"Normal", "Type":"Fire","Moves":mm}
+   ["Lava Plume",80,80,4,"\nBurned - 30%"]]
+Magmortar = {"speed":83, "HP":240,"HP_Cur":240, "Type":"Fire","Moves":mm}
 
 lm=[["Leaf Blade",90,70,2,""],
-   ["Synthesis",0,100,4,"Regen - 33%"],
+   ["Synthesis",0,100,4,"\nRegen - 33%"],
    ["Quick Attack",40,100,5,""],
    ["Magical Leaf",60,90,2,""]]
-Leafeon = {"speed":95, "HP":223,"HP_Cur":223, "Condition":"Normal", "Type":"Grass","Moves":lm}
+Leafeon = {"speed":95, "HP":223,"HP_Cur":223, "Type":"Grass","Moves":lm}
 
 dm=[["Sucker Punch",70,90,3,""],
-   ["Razor Leaf",55,95,3,"Critical Hit - 12.5%"],
+   ["Razor Leaf",55,95,3,"\nCritical Hit - 12.5%"],
    ["Peck",35,90,5,""],
    ["Leafage",40,90,4,""]]
-Decidueye = {"speed":70, "HP":245,"HP_Cur":245, "Condition":"Normal", "Type":"Grass","Moves":dm}
+Decidueye = {"speed":70, "HP":245,"HP_Cur":245, "Type":"Grass","Moves":dm}
 
 vm=[["Petal Blizzard",90,70,1,""],
    ["Tackle",40,90,5,""],
    ["Vine Whip",45,90,5,""],
    ["Take Down",90,85,2,""]]
-Venusaur = {"speed":80, "HP":249,"HP_Cur":249, "Condition":"Normal", "Type":"Grass","Moves":vm}
+Venusaur = {"speed":80, "HP":249,"HP_Cur":249, "Type":"Grass","Moves":vm}
 
 gm=[["Water Pulse",60,95,4,""],
    ["Night Slash",70,80,4,""],
    ["Hydro Pump",90,80,1,""],
    ["Quick Attack",40,100,5,""]]
-Greninja = {"speed":122, "HP":235,"HP_Cur":235, "Condition":"Normal", "Type":"Water","Moves":gm}
+Greninja = {"speed":122, "HP":235,"HP_Cur":235, "Type":"Water","Moves":gm}
 
-gym=[["Twister",40,100,5,"Flinch - 30%"],
+gym=[["Twister",40,100,5,"\nFlinch - 30%"],
    ["Ice Fang",65,95,4,""],
    ["Aqua Tail",90,80,2,""],
    ["Blizzard",90,70,2,""]]
-Gyarados = {"speed":81, "HP":274,"HP_Cur":274, "Condition":"Normal", "Type":"Water","Moves":gym}
+Gyarados = {"speed":81, "HP":274,"HP_Cur":274, "Type":"Water","Moves":gym}
 
 bm=[["Water Gun",40,100,5,""],
-   ["Bite",60,90,4,"Flinch - 30%"],
+   ["Bite",60,90,4,"\nFlinch - 30%"],
    ["Skull Bash",90,90,2,""],
    ["Flash Cannon",80,80,2,""]]
-Blastoise = {"speed":78, "HP":247,"HP_Cur":247, "Condition":"Normal", "Type":"Water","Moves":bm}
+Blastoise = {"speed":78, "HP":247,"HP_Cur":247, "Type":"Water","Moves":bm}
+
+mom=[["Gust",40,100,5,""],
+   ["Ancient Power",60,90,4,""],
+   ["Incinerate",60,90,2,""],
+   ["Heat Wave",95,80,1,"\nBurned - 10%"]]
+Moltres = {"speed":90, "HP":290,"HP_Cur":290, "Type":"Fire","Moves":mom}
+
+sm=[["Extrasensory",80,90,2,"\nFlinch - 10%"],
+   ["Surf",90,90,2,""],
+   ["Water Gun",40,100,5,""],
+   ["Facade",70,90,2,""]]
+Suicune = {"speed":85, "HP":310,"HP_Cur":310, "Type":"Water","Moves":sm}
+
+fm=[["Metal Claw",50,95,4,""],
+   ["Power Whip",120,80,2,""],
+   ["Knock Off",65,90,3,""],
+   ["Tackle",40,100,5,""]]
+Ferrothorn = {"speed":20, "HP":258,"HP_Cur":258, "Type":"Grass","Moves":fm}
+
+rm=[["Spark",65,90,4,"\nFlinch - 30%"],
+   ["Zap Cannon",120,80,1,""],
+   ["Thunder Shock",40,100,5,"\nFlinch - 10%"],
+   ["Howl",0,100,5,"\nUser's Attack - 10"]]
+Raikou = {"speed":115, "HP":290,"HP_Cur":290, "Type":"Electric","Moves":rm}
+
+manm=[["Tackle",40,100,5,""],
+   ["Discharge",80,95,2,"\nFlinch - 30%"],
+   ["Thunder Fang",65,95,4,"\nFlinch - 10%"],
+   ["Quick Attack",40,100,5,""]]
+Manectric = {"speed":105, "HP":250,"HP_Cur":250, "Type":"Electric","Moves":manm}
+
+elm=[["Rising Voltage",70,90,3,""],
+   ["Thunder Punch",75,95,2,""],
+   ["Quick Attack",40,100,5,""],
+   ["Thunder",110,80,2,"\nFlinch - 30%"]]
+Electivire = {"speed":95, "HP":260,"HP_Cur":260, "Type":"Electric","Moves":elm}
+
+zm=[["Pluck",60,95,4,""],
+   ["Drill Peck",80,90,2,""],
+   ["Zap Cannon",120,80,1,""],
+   ["Thunderbolt",90,85,3,"\nFlinch - 10%"]]
+Zapdos = {"speed":100, "HP":290,"HP_Cur":290, "Type":"Electric","Moves":zm}
+
+tm=[["Rock Slide",75,90,3,"\nFlinch - 30%"],
+   ["Stone Edge",100,80,3,"\nCritical Hit - 12.5%"],
+   ["Stomp",65,100,3,""],
+   ["Scary Face",0,100,5,"\nDecrease in Rival Pokemon's Speed by 25"]]
+Tyranitar = {"speed":61, "HP":310,"HP_Cur":310, "Type":"Rock","Moves":tm}
+
+lycm=[["Howl",0,100,5,"\nUser's Attack - 10"],
+   ["Accelerock",40,100,5,""],
+   ["Rock Throw",50,90,5,""],
+   ["Rock Tomb",60,95,3,"\nDecrease in Rival Pokemon's Speed by 15"]]
+Lycanroc = {"speed":112, "HP":260,"HP_Cur":260, "Type":"Rock","Moves":lycm}
+
+gigam=[["Smack Down",50,95,5,""],
+   ["Power Gem",80,90,3,""],
+   ["Tackle",40,100,5,""],
+   ["Take Down",90,85,4,"\nRecoil - 25%"]]
+Gigalith = {"speed":25, "HP":280,"HP_Cur":280, "Type":"Rock","Moves":gigam}
+
+rhym=[["Horn Attack",65,95,3,""],
+   ["Rock Slide",75,90,3,"\nFlinch - 30%"],
+   ["Scary Face",0,100,5,"\nDecrease in Rival Pokemon's Speed by 25"],
+   ["Earthquake",100,90,2,""]]
+Rhyperior = {"speed":40, "HP":340,"HP_Cur":340, "Type":"Rock","Moves":rhym}
